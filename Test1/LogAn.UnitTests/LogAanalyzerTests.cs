c45062fd059f08b7ac621a70420bc139db4e4e9c
Васил <v.gutev36@gmail.com>
2017-01-28 19:05:51 +0200

    Added work on chapter 1 & 2

---
+++ Test1/LogAn.UnitTests/LogAanalyzerTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAanalyzerTests
+    {
+        [Test]
+        public void IsValidLogFileName_BadExtension_ReturnFalse()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName("alabalaBadExtension.foo");
+
+            Assert.False(result);
+        }
+
+        [TestCase("filewithgoodextension.slf")]
+        [TestCase("filewithgoodextension.SLF")]
+        public void IsValidLogFileName_Validxtension_ReturnsTrue(string file)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result =analyzer.IsValidLogFileName(file);
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsValidFileName_EmptyFileName_Throws()
+        {
+            LogAnalyzer la = new LogAnalyzer();
+
+            var ex = Assert.Catch<Exception>(() => la.IsValidLogFileName(""));
+
+            StringAssert.Contains("filename has to be provided", ex.Message);
+        }
+
+        [TestCase("badname.foo", false)]
+        [TestCase("goodname.slf", true)]
+        public void IsValidFileName_WhenCalled_ChangesWasLasFileNameValid(string filename, bool expected)
+        {
+            LogAnalyzer la = new LogAnalyzer();
+
+            la.IsValidLogFileName(filename);
+
+            Assert.AreEqual(expected, la.WasLastFileNameValid);
+        }
+
+        [Test]
+        public void Sum_ByDefault_ReturnsZero()
+        {
+            MemCalculator calc = new MemCalculator();
+
+            int lastSum = calc.Sum();
+
+            Assert.AreEqual(0, lastSum);
+        }
+
+        [Test]
+        public void Add_WhenCalled_ChangesSum()
+        {
+            MemCalculator calc = new MemCalculator();
+
+            calc.Add(5);
+            int sum = calc.Sum();
+
+            Assert.AreEqual(5, sum);
+        }
+    }
+}
